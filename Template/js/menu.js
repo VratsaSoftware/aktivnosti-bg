a2dcb6cca5dd65bcb79bbd7206439bbfb657ba93
Angel <achi_iv@abv.bg>
2019-03-01 15:12:14 +0200

    add org.html, schedue.html, news.html

--- Template/js/menu.js
+++ Template/js/menu.js
@@ -50,3 +50,7 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
