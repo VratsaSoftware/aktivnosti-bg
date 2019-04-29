a9f028896d1cfceac22c81ac5808195ba48c2d72
Borislav Krastev <bp.krastev@gmail.com>
2019-04-29 18:15:45 +0300

    admin panel navbar toggle button fixed

--- Laravel/resources/views/layouts/admin.blade.php
+++ Laravel/resources/views/layouts/admin.blade.php
@@ -23,6 +23,12 @@
     <div id="wrapper">
         <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand" href="{{ URL::to('/') }}">{{ config('app.name', 'Aktivnosti-bg') }}</a>
             </div>
             <div style="color: white;padding: 15px 50px 5px 50px;float: right;font-size: 16px;">
