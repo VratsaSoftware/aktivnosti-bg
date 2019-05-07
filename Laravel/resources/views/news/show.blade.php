0a6ca7a456fa258dea4f54cc513d7087586248fc
Angel <achi_iv@abv.bg>
2019-05-07 12:58:11 +0300

    news show.blade.php

--- Laravel/resources/views/news/show.blade.php
+++ Laravel/resources/views/news/show.blade.php
@@ -1,55 +1,100 @@
 
 @extends('layouts.master')
 
-@section('title', 'Новини')
+@section('title', $news->heading)
 
 @section('content')
-	<!-- main-container -->		
-	<div class=" container main-container">
-		<!-- itemslimits -->
-		<div class="container col-md-12"> 
-			<!-- single news-->
-			<div class="col-md-4 col-sm-6 service-box">
-				<!-- service-box -->
-				@foreach($news->photos as $photo)
-				<img src="{{ asset('user_files/images/news/'.$photo->image_path)}}" alt="{{ $photo->description }}">
-				@endforeach
-				<h3>{{$news->heading}}</h3>
-				<div class="h-10"></div>
-				<!-- end service-box -->
-				<!-- Trigger the modal with a button -->
-				<button type="button" class="btn btn-box" data-toggle="modal" data-target="#myModal">Прочети</button>
-				<div class="modal fade" id="myModal" role="dialog">
-					<div class="modal-dialog">
-						<!-- Modal content-->
-						<div class="modal-content">
-						<!-- Modal -->
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal">&times;</button>
-								<h3>{{$news->heading}}</h3>
-							</div>
-							<div class="modal-body">
-								<div class="col-md-6 col-sm-6">
-									@foreach($news->photos as $photo)
-									<img src="{{ asset('user_files/images/news/'.$photo->image_path)}}" alt="{{ $photo->description }}">
-									@endforeach
-								</div>
-								<div class="col-md-6 col-sm-6">
-									<p>{{$news->description}} </p>
-								</div>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-box" data-dismiss="modal">Затвори</button>
+<!-- main-container -->
+    <div class="container main-container">
+		<!-- left side -->
+        <div class="col-md-5 col-sm-6 col-xs-12">
+
+            <!--pictures from Adobe Stock-->
+            <div class="news_img">
+			@foreach($news->photos as $photo)
+				<img src="{{ asset('user_files/images/news/'.$photo->image_path)}}" alt="{{ $photo->description }}">			
+			@endforeach
+            </div>
+			<!--social list-->
+			<ul class="social-ul">
+				<li class="box-social"><a href="#0"><i class="fab fa-facebook-f"></i></a></li>
+				<li class="box-social"><a href="#0"><i class="fab fa-instagram"></i></a></li>
+				<li class="box-social"><a href="#0"><i class="fab fa-google-plus-g"></i></a></li>
+				<li class="box-social"><a href="#0"><i class="fab fa-twitter"></i></a></li>
+			</ul>
+        </div>
+		<!-- end left side -->
+		<!-- right side -->
+        <div class="col-md-7 col-sm-6 col-xs-12">
+            <h2 class="org">{{$news->heading}}</h2>
+			@if($news->article_type::first()->getTable()!='categories')
+			<a href="{{ route(''.$news->article_type::first()->getTable().'.show', $news->article_id) }}">@endif<h5 class="org">{{ $news->article_type::find($news->article_id)->name }}</h5>@if($news->article_type::first()->getTable()!='categoories')</a>@endif
+            <ul class="cat-ul">
+                <li><i class="fas fa-calendar-alt"></i>{{Carbon\Carbon::parse($news->date)->format('j.m.Y')}}</li>
+            </ul>
+			<p class="description-text">{{$news->description}}</p>
+        </div>
+		<!-- end right side -->
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<div class="h-section">
+				<img src="{{asset('img/portfolio/fav.png')}}" alt="logo" class="logo-section">
+				<h4 class="h-activity"><span>Снимки</span></h4>	
+			</div>
+			<div class="gallery-container">
+				<div class="tz-gallery">
+					<div class="tz">
+						@foreach($news->photos as $photo)
+						<div class="col-xs-6 col-sm-6 col-md-4">
+							<div class="marg">
+								<a class="lightbox" href="{{ asset('user_files/images/news/'.$photo->image_path)}}">
+									<img src="{{ asset('user_files/images/nesw/'.$photo->image_path)}}" alt="image" class="img-responsive" />
+								</a>
 							</div>
 						</div>
+						@endforeach						   
 					</div>
-					{{$news->article_type}}
 				</div>
-			</div>
-			<!-- end single news-->
-		</div> 
-	</div>
-    <!-- end main-container -->
+			</div>	
+		</div>
+    </div>
+	<script>
+	$(function() {
+  $(".img-w").each(function() {
+    $(this).wrap("<div class='img-c'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+     $(this).css('background-image', 'url(' + imgSrc + ')');
+  })
+            
+  
+  $(".img-c").click(function() {
+    let w = $(this).outerWidth()
+    let h = $(this).outerHeight()
+    let x = $(this).offset().left
+    let y = $(this).offset().top
+    
+    
+    $(".active").not($(this)).remove()
+    let copy = $(this).clone();
+    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+    $(".active").css('top', y - 8);
+    $(".active").css('left', x - 8);
+    
+      setTimeout(function() {
+    copy.addClass("positioned")
+  }, 0)
+    
+  }) 
+  
+})
 
+$(document).on("click", ".img-c.active", function() {
+  let copy = $(this)
+  copy.removeClass("positioned active").addClass("postactive")
+  setTimeout(function() {
+    copy.remove();
+  }, 500)
+})
+	</script>
+    <!-- end main-container -->
 @endsection
 						
