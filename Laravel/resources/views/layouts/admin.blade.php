2e061397a5f48eabd1806774131f2ff068078db1
Angel <achi_iv@abv.bg>
2019-06-22 17:30:25 +0300

    google analytics

--- Laravel/resources/views/layouts/admin.blade.php
+++ Laravel/resources/views/layouts/admin.blade.php
@@ -2,6 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-51734359-9"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){
+			dataLayer.push(arguments);
+			}
+		gtag('js', new Date());
+		gtag('config', 'UA-51734359-9');
+	</script>	
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ config('app.name', 'Aktivnosti-bg') }}</title>
