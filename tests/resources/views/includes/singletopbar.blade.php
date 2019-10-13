05d4acf1244737d8f007c005b9371be42f17d44f
unknown <bp.krastev@gmail.com>
2019-10-14 00:04:12 +0300

    Cleaning "Test" folder of project

--- tests/resources/views/includes/singletopbar.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="top-bar single-project">
-            <h1>@yield('title')</h1>
-            <p><a href="{{route('activities.index')}}">Намери активност</a> / @yield('title')</p>
-        </div>
--- tests/resources/views/includes/topbar.blade.php
+++
@@ -1,7 +0,0 @@
-    <!-- top bar -->
-    <div class="top-bar">
-        <h1 id="message">Търсиш<span>?</span><!-- <img src="img/logo_large.png" width="200" alt="Logo"> -->
-			</h1>
-        <p><a href="{{route('activities.index')}}">Приключенията във Враца са тук!</a></p>
-    </div>
-    <!-- end top bar -->
