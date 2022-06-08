f86adaa3f63b7e7cb7e490fb2c6ae17ffcdb900b
georgi2003 <georgidim30@gmail.com>
2022-06-08 10:05:31 +0300

    Fix text

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -1,7 +1,7 @@
-    <!-- top bar -->
-    <div class="top-bar">
-        <h1 id="message">Търсиш<span>?</span><!-- <img src="img/logo_large.png" width="200" alt="Logo"> -->
-			</h1>
-        <p><a href="{{route('activities.index')}}">Приключенията във Враца са тук!</a></p>
-    </div>
-    <!-- end top bar -->
+<!-- top bar -->
+<div class="top-bar">
+    <h1 id="message">Търсиш<span>?</span><!-- <img src="img/logo_large.png" width="200" alt="Logo"> -->
+    </h1>
+    <p><a href="{{ route('activities.index') }}">Приключенията във вашия град са тук!</a></p>
+</div>
+<!-- end top bar -->
