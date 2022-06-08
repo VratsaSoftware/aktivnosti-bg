eaf51826e5b64bd2fef48b03b5b9534575c07c15
georgi2003 <georgidim30@gmail.com>
2022-06-08 17:18:18 +0300

    Fix arrow-up icon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -71,7 +71,7 @@
 @include('includes.footer')
 
 <!-- back to top -->
-<a href="#0" class="cd-top"><i class="ion-android-arrow-up"></i></a>
+<a href="#0" class="cd-top"><i class="fas fa-arrow-up"></i></a>
 <!-- end back to top -->
 
 <!-- jQuery -->
